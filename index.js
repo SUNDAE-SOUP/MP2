9bc33594114ba5292419771893226ee7bc783bac
Rhejoel Joson <rhejoelstrong@gmail.com>
2023-04-28 04:02:50 +0800

    with npm packages
    Install npm packages and update the product html code.

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejs = require('html');
+
+//templating engine
+
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+//images
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+
+
+/* app.use('/', require('./route')); */
+
+
+
+app.listen(3000, () => {
+    console.log('All Goods at port 3000')
+})
